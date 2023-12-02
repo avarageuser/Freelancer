cd9085b68909c1e0bf13cb164074777de366b91d
avarageuser <gonzalezmoraleslucas@gmail.com>
2023-12-01 22:27:43 -0300

    pagina terminada

---
+++ script.js
@@ -0,0 +1,18 @@
+/* Función para mostrar u ocultar el dropdown */
+function botonsrop() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+/* Cierra el dropdown si el usuario hace clic fuera de él */
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
